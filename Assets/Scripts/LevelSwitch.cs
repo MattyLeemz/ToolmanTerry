6342cf367bca5a7b17ffacc266775edc61f54857
Matthew Lima <ml396@njit.edu>
2018-11-01 18:25:46 -0400

    Ninth Commit

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
