572b75012a044b09d4fa804719a1b1878caa7b08
Matthew Lima <ml396@njit.edu>
2018-11-01 00:04:02 -0400

    Fifth Commit

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
     public int health = 100;
+    [SerializeField]
+    GameObject enemyBullet;
+
+    float fireRate;
+    float nextFire;
+
+    void Start()
+    {
+        fireRate = 1f;
+        nextFire = Time.time;
+    }
+
+    void Update()
+    {
+        CheckIfTimeToFire();
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(enemyBullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
 
     public GameObject deathEffect;
 
--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour {
 
-	public static void KillPlayer (Player player)
+    public static GameMaster gm;
+
+    void Start()
+    {
+     if(gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("spawn").GetComponent<GameMaster>();
+        }  
+    }
+    public Transform player;
+    public Transform spawnPoint;
+
+    public void RespawnPlayer()
+    {
+        Instantiate(player, spawnPoint.position, spawnPoint.rotation);
+    }
+    public static void KillPlayer(Player player)
     {
         Destroy(player.gameObject);
+        
     }
 }
